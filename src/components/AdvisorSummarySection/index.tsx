869987ae6da266b14f082035cd6e6b817a4afac4
bernardo <bernardo@socarrao.com.br>
2025-03-31 21:17:59 -0300

    finishing Advisor Summary Section

--- src/components/AdvisorSummarySection/index.tsx
+++ src/components/AdvisorSummarySection/index.tsx
@@ -1,4 +1,4 @@
-import { FaSuitcase } from "react-icons/fa";
+import Suitcase from "../../assets/suitcase.png";
 import Desktop from "../../assets/desktop.png";
 import Pencil from "../../assets/pencil.png";
 import FileText from "../../assets/file-text.png";
@@ -28,7 +28,7 @@ export const AdvisorSummarySection = () => {
                 </div>
             <div className="container md">
                 <h3 className="title six">1. What is PromoAI</h3>
-                <div>
+                <div className={style.flexContainer}>
                     <div>
                         <h4  className="title seven">ABOUT THE PRODUCT</h4>
                         <p className="paragraph nine">A single platform for all promotional management, {" "}
@@ -42,33 +42,53 @@ export const AdvisorSummarySection = () => {
                     </a>{" "} 
                     is broken into three key modules: Promotion Analyzer, Promotion Planner, and Promotion Advisor. 
                        </p>
-                        <p className="paragraph nine">The app aims to {" "}<span  style={{ fontWeight: "bold" }}>{" "}assist retailers in better planning, executing, and analyzing promotions.</span>{" "}</p>
+                       <p className={`${"paragraph nine"} ${style.secondParagraph}`}>
+                            The app aims to{" "}
+                            <span style={{ fontWeight: "bold" }}>
+                            assist retailers in better planning, executing, and analyzing promotions.
+                            </span>
+                        </p>
                     </div>
                     <div>
                         <h4  className="title seven"> MODULE GOALS & PURPOSE</h4>
                         <p className="paragraph nine">The new module is designed to {" "}<span style={{ fontWeight: "bold" }}>optimize promotions</span>, helping users achieve their goals related to <span style={{ fontWeight: "bold" }}>sales, quantity, revenue, or margin.</span></p>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <span><FaSuitcase size={20} color="#5F6D7E" /> TYPE</span>
-                        <p>Contract</p>
+                <div className={style.grid}>
+                    <div className={style.box}>
+                    <div className={style.flexItem}>
+                        <span className="paragraph ten">TYPE</span>
+                        <img src={Suitcase}></img>
+                     </div>
+                    <p className="paragraph seven">Contract</p>
                     </div>
-                    <div>
-                        <span><img src={Desktop}/>TOOLS</span>
-                        <p>Figma, Confluence, JIRA</p>
+                    <div className={style.box}>
+                        <div className={style.flexItem}>
+                            <span className="paragraph ten">TOOLS</span>
+                            <img src={Desktop}/>
+                        </div>
+                        <p className="paragraph seven">Figma, Confluence, JIRA</p>
                     </div>
-                    <div>
-                        <span><img src={Pencil}/>ROLE</span>
-                        <p>UX Designer</p>
+                    <div className={style.box}>
+                        <div className={style.flexItem}>
+                            <span className="paragraph ten">ROLE</span>
+                            <img src={Pencil}/>
+                        </div>
+                        <p className="paragraph seven">UX Designer</p>
                     </div>
-                    <div>
-                        <span><img src={FileText}/>SCOPE</span>
-                        <p>New Module on App Platform</p>
+                    <div className={style.box}>
+                        <div className={style.flexItem}>
+                            <span className="paragraph ten">SCOPE</span>
+                            <img src={FileText}/>
+                        </div>
+                        <p className="paragraph seven">New Module on App Platform</p>
                     </div>
-                    <div>
-                        <span><img src={Vector}/>TEAM</span>
-                        <p>Full Cross-functional Team</p>
+                    <div className={style.box}>
+                        <div  className={style.flexItem}>
+                            <span className="paragraph ten">TEAM</span>
+                            <img src={Vector}/>
+                        </div>
+                        <p className="paragraph seven">Full Cross-functional Team</p>
                     </div>
                 </div>
             </div>
